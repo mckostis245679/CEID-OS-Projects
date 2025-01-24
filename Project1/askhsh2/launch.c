907c25598bff8d001f29daa8f2cf1eeedc563683
Konstantinos Loukanaris <kostasloukas412@gmail.com>
2025-01-24 19:24:08 +0200

    working askhsh 1,2,3

--- Project1/askhsh2/launch.c
+++ Project1/askhsh2/launch.c
@@ -1,7 +1,7 @@
    #include "ipc_utils.h"
 
-    #define MAX_PASSENGERS 100
-    #define MAX_BOATS 10
+    #define MAX_PASSENGERS 200
+    #define MAX_BOATS 50
 
     sem_t boat_space;
     sem_t boarding_queue;
@@ -13,18 +13,19 @@
 
         int passengers, boats, seats_per_boat;
 
-        // Eisagwgh arithmou epibatwn
+        
         printf("Enter number of passengers: ");
         scanf("%d", &passengers);
-        printf("Enter number of boats: ");  // Eisagwgh arithmou lemvou
+        printf("Enter number of boats: "); 
         scanf("%d", &boats);
-        printf("Enter seats per boat: ");    //eisagwgh thesewn ana lemvo
+        printf("Enter seats per boat: "); 
         scanf("%d", &seats_per_boat);
 
-        // Elegxos orion
+       
+
         if (passengers > MAX_PASSENGERS || boats > MAX_BOATS || seats_per_boat <= 0) {
             printf("Invalid input! Please check the limits.\n");
-            return EXIT_FAILURE;
+            return 1;
         }
 
         int queue[passengers];
@@ -35,12 +36,12 @@
             queue[i] = i + 1;
         }
 
-        // Arxikopoihsh semaphorewn
+        // Arxikopoihsh semaphorwn
         init_semaphore(&boat_space, seats_per_boat);
         init_semaphore(&boarding_queue, 1);
         init_semaphore(&boats_available, boats);
 
-        // Kyklos gia thn eksypiretisi twn epibatwn
+        
      while (queue_size > 0) {
             int passenger_id = queue[0];
 
@@ -56,24 +57,26 @@
 
             //kai paei sto telos
             queue[queue_size - 1] = passenger_id;
+
             } else {
             // Kalesma synarthshs gia thn diadikasia epibivashs
             passenger_routine(passenger_id, passengers, boats, seats_per_boat);
 
-            //epeidh sthn arxh pairnoume ton prwto epibath (queue[0]) gia na ton pame pisw an xreiastei, an den allaksei gnwmh shiftaroume gia na pame ston epomeno (queue[1])
+            //kathe fora pairnw to passenger_id ws queue[0] ara to front, kai afou phra to front shiftarw gia na paei o epomenos
             for (int j = 0; j < queue_size - 1; j++) {
                 queue[j] = queue[j + 1];
             }
-            //paei epivivasthke
+
+       
             queue_size--;
             }
     }
 
-    //cleanup tous shmaforous
+    
     destroy_semaphore(&boat_space);
     destroy_semaphore(&boarding_queue);
     destroy_semaphore(&boats_available);
 
         printf("All passengers have been rescued!\n");
-        return EXIT_SUCCESS;
+        return 0;
 }
--- Project1/askhsh2/passenger.c
+++ Project1/askhsh2/passenger.c
@@ -7,73 +7,80 @@ extern sem_t boats_available;
 void init_semaphore(sem_t *sem, int value) {
     if (sem_init(sem, 0, value) != 0) {
         perror("sem_init failed");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 }
 
 void destroy_semaphore(sem_t *sem) {
     if (sem_destroy(sem) != 0) {
         perror("sem_destroy failed");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 }
 
 void wait_semaphore(sem_t *sem) {
     if (sem_wait(sem) != 0) {
         perror("sem_wait failed");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 }
 
 void post_semaphore(sem_t *sem) {
     if (sem_post(sem) != 0) {
         perror("sem_post failed");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 }
 
+
 void passenger_routine(int passenger_id, int total_passengers, int totalboats, int seats_per_boat) {
-    static int remaining_passengers = -1;  // Arxika to remaining_passengers einai -1 gia na ginei h arxikopoiisi
+    static int remaining_passengers = -1;
     static int passengers_in_boat = 0;    
     static int boats_filled = 0;          
     
     if (remaining_passengers == -1) {
-        remaining_passengers = total_passengers;  // an to remaining_passengers einai -1, arxikopoioume me to synoliko plithos epibaton
+        remaining_passengers = total_passengers;
     }
 
     if (remaining_passengers <= 0) {
-        return;  //An den exoun apomeinei epibates, termatizei h diadikasia
+        return;
     }
 
-    wait_semaphore(&boarding_queue);  //perimenoume seira gia na kanoume thn epibivasi
+    wait_semaphore(&boarding_queue);
 
     if (remaining_passengers > 0) {
-        wait_semaphore(&boats_available);  // Perimenoume na ginei diathesimh mia lemvos
-        wait_semaphore(&boat_space);       // Perimenoume na vrethoun thesies sth lemvo
+        wait_semaphore(&boats_available);
+        wait_semaphore(&boat_space);
 
         remaining_passengers--; 
         printf("Passenger %d boards the boat no %d. Remaining passengers: %d\n", passenger_id, boats_filled+1 ,remaining_passengers);
 
-        passengers_in_boat++; //bainei autos o enas
+        passengers_in_boat++;
 
-        if (passengers_in_boat == seats_per_boat || remaining_passengers == 0) {
-            boats_filled++;  //an eixe gemisei, enhmerwnoume ta boats_filled
-            for (int i = 0; i < totalboats; i++) {
-                post_semaphore(&boats_available);  //h lemvos einai available pali
-            }
-            for (int i = 0; i < passengers_in_boat; i++) {
-                post_semaphore(&boat_space);  //post gia na apeleytherwsoume tis theseis sth lemvo
-            }
-
-            if (boats_filled == totalboats || remaining_passengers == 0) {
+        if (passengers_in_boat == seats_per_boat) {
+            
+            boats_filled++;
+            
+            if (boats_filled == totalboats) {
                 printf("%d boat(s) depart(s) with %d passengers each.\n", boats_filled, passengers_in_boat);
                 sleep(2); 
                 printf("The boats have returned.\n");
-                boats_filled = 0;  //hrthan pisw ara pali apthn arxh
+                boats_filled = 0;
+                 for (int i = 0; i < totalboats; i++) {
+                      post_semaphore(&boats_available);
+                 }
             }
-            passengers_in_boat = 0;  //epanafora tou arithmou epibaton
+              for (int i = 0; i < passengers_in_boat; i++) {
+                 post_semaphore(&boat_space);
+               }       
+            passengers_in_boat = 0;
+        } else {
+            post_semaphore(&boats_available);
+            post_semaphore(&boat_space);
         }
     }
 
-    post_semaphore(&boarding_queue);  //post gia na mpei o epomenos epibaths
+    post_semaphore(&boarding_queue);
 }
+
+
